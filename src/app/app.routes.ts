b6f6c97975ce1fb9f832357f084ad6518f29245f
yohagos <yosef.hagos@googlemail.com>
2025-09-29 19:16:34 +0200

    adjusting app component and global styles.scss

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Overview } from './features/overview/overview';
+import { Game } from './features/game/game';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Overview,
+  },
+  {
+    path: 'game',
+    component: Game,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Toolbar } from './shared/toolbar/toolbar';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    Toolbar,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
