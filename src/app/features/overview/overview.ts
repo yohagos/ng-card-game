e72d00e1e442fd3f585a10b7e38811339734e21c
yohagos <yosef.hagos@googlemail.com>
2025-09-29 19:18:01 +0200

    adding overview component as landing page

---
+++ src/app/features/overview/overview.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Card } from '../../shared/card/card';
+
+@Component({
+  selector: 'app-overview',
+  imports: [
+    Card,
+  ],
+  templateUrl: './overview.html',
+  styleUrl: './overview.scss'
+})
+export class Overview {
+
+}
