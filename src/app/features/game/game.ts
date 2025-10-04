6a145e874898b23c7a2c0b7ba19879e68ed3238e
yohagos <yosef.hagos@googlemail.com>
2025-10-04 21:26:29 +0200

    refactoing imports and behavior issues

--- src/app/features/game/game.ts
+++ src/app/features/game/game.ts
@@ -1,9 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { StartNewGameModal } from '../../shared/start-new-game-modal/start-new-game-modal';
-import { ScoreModal } from '../../shared/score-modal/score-modal';
-import { SelectPointsModal } from '../../shared/select-points-modal/select-points-modal';
+import { StartNewGameModal } from '../../shared/modals/start-new-game-modal/start-new-game-modal';
+import { ScoreModal } from '../../shared/modals/score-modal/score-modal';
+import { SelectPointsModal } from '../../shared/modals/select-points-modal/select-points-modal';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
@@ -24,19 +24,23 @@ export class Game {
 
     console.log('clicked on restart')
 
-    dialogRef.afterClosed().subscribe(result => {
+    /* dialogRef.afterClosed().subscribe(result => {
       console.log(result)
-    })
+    }) */
+    console.log('CurrentState of the dialog => ', dialogRef.getState())
   }
 
   showScore() {
     const dialogRef = this.dialog.open(ScoreModal)
-    console.log('clicked on restart')
   }
 
   selectPoints() {
     const dialogRef = this.dialog.open(SelectPointsModal)
 
-    dialogRef.afterClosed().subscribe(result => console.log(result))
+    /* dialogRef.afterClosed().subscribe(result => {
+      console.log("Selected Points by User => ",result)
+    }) */
+
+    console.log('CurrentState of the dialog => ', dialogRef.getState())
   }
 }
