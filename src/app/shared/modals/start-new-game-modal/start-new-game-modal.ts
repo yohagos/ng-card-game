5bd0274af1f1b29235b3830d998126165591aac8
yohagos <yosef.hagos@googlemail.com>
2025-10-04 21:24:51 +0200

    minor changes for start-new-game-modal

---
+++ src/app/shared/modals/start-new-game-modal/start-new-game-modal.ts
@@ -0,0 +1,21 @@
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-start-new-game-modal',
+  imports: [
+    MatButtonModule,
+    MatDialogModule,
+  ],
+  templateUrl: './start-new-game-modal.html',
+  styleUrl: './start-new-game-modal.scss'
+})
+export class StartNewGameModal {
+  readonly dialogRef = inject(MatDialogRef<StartNewGameModal>)
+
+  restartGame() {
+    console.log('Restart Game was chosen!')
+    this.dialogRef.close()
+  }
+}
