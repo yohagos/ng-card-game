8689e28314dcc3d00553259d7ae34fe884063a57
yohagos <yosef.hagos@googlemail.com>
2025-10-09 19:59:16 +0200

    cleaning up overview and currently using as testing ground for modals or small features

--- src/app/features/overview/overview.ts
+++ src/app/features/overview/overview.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
-import { Card } from '../../shared/card/card';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { TrumpModal } from '../../shared/modals/trump-modal/trump-modal';
+
 
 @Component({
   selector: 'app-overview',
   imports: [
-    Card,
+    MatButtonModule,
   ],
   templateUrl: './overview.html',
   styleUrl: './overview.scss'
 })
 export class Overview {
-
+  private dialog = inject(MatDialog)
+  openTrumpModal() {
+    this.dialog.open(TrumpModal)
+  }
 }
