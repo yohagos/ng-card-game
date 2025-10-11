4ca40641bf674da37bbbdfbfba94614f8509cde6
yohagos <yosef.hagos@googlemail.com>
2025-10-11 21:36:26 +0200

    refactoring game board component

--- src/app/features/game/game.ts
+++ src/app/features/game/game.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { StartNewGameModal } from '../../shared/modals/start-new-game-modal/start-new-game-modal';
@@ -9,9 +9,12 @@ import { CdkDropList, CdkDrag, CdkDragDrop, moveItemInArray, transferArrayItem }
 import { CommonModule } from '@angular/common';
 import { Trump, TrumpModal } from '../../shared/modals/trump-modal/trump-modal';
 import { BidPanel } from './bid-panel/bid-panel';
-import { BiddingService } from './bid-panel/bidding-service';
-import { TrickService } from './trick-service';
+import { BiddingService, Seat } from './bid-panel/bidding-service';
+import { PlayedCard, TrickService } from './trick-service';
 import { GameValidatorService } from './game-validator-service';
+import { Card, DeckService, PlayerHands } from './deck.service';
+import { CardComponent } from '../../shared/card/card';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-game',
@@ -21,9 +24,9 @@ import { GameValidatorService } from './game-validator-service';
     MatIconModule,
     CommonModule,
     CdkDropList,
-    CdkDrag,
 
     BidPanel,
+    CardComponent,
   ],
   templateUrl: './game.html',
   styleUrl: './game.scss'
@@ -36,8 +39,21 @@ export class Game implements OnInit, OnDestroy {
   biddingService = inject(BiddingService)
   private trickService = inject(TrickService)
   private validatorService = inject(GameValidatorService)
+  private deckService = inject(DeckService)
 
-  restartGame() {
+  hands = signal<PlayerHands>({
+    bottom: [],
+    left: [],
+    top: [],
+    right: [],
+  })
+  trick = signal<PlayedCard[]>([])
+
+  trickCards = computed(() => this.trick().map(p => p.card))
+
+  seats: Seat[] = ['bottom', 'right', 'top', 'left']
+
+  /* restartGame() {
     const dialogRef = this.dialog.open(StartNewGameModal)
   }
 
@@ -47,54 +63,23 @@ export class Game implements OnInit, OnDestroy {
 
   selectPoints() {
     const dialogRef = this.dialog.open(SelectPointsModal)
-  }
-
-    user_a = signal(<any>[])
-  user_b = signal(<any>[])
-  user_c = signal(<any>[])
-  user_d = signal(<any>[])
-  centerCards: any[] = []
-
-  /* suits = ['♠', '♥', '♦', '♣']
-  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-
-  loading = signal(true)
-
-  cards: any[] = []
-  randomCards: any[] = []
-
-  userA: any[] = []
-  userB: any[] = []
-  userC: any[] = []
-  userD: any[] = []
-
-  user_a = signal(<any>[])
-  user_b = signal(<any>[])
-  user_c = signal(<any>[])
-  user_d = signal(<any>[])
-
-  centerCards: any[] = [] */
-
-/*   constructor() {
-    this.suits.forEach(suit => {
-      this.values.forEach(value => {
-        this.cards.push({suit, value})
-      })
-    })
   } */
 
   ngOnInit() {
-    /* this.getRandomCards()
-    this.loading.set(false) */
+    this.hands.set(this.deckService.deal())
 
     this.biddingService.startBidding('bottom')
-    this.biddingService.biddingEnd$.subscribe(({winner, bid}) => {
+    this.biddingService.biddingEnd$.subscribe(async ({winner, bid}) => {
+      const suit = await this.chooseTrump()
+      this.trickService.startTrick(suit)
+      this.validatorService.startTrick(suit, null)
+
       console.log('Bidding Process => ', winner, bid)
     })
 
-    const trump = '♠'
+    /* const trump = '♠'
     this.trickService.startTrick(trump)
-    this.validatorService.startTrick(trump, null)
+    this.validatorService.startTrick(trump, null) */
   }
 
   /* valueOrder: {[key: string]: number} = {
@@ -107,22 +92,22 @@ export class Game implements OnInit, OnDestroy {
     '♠': 1, '♥': 2, '♣': 3, '♦': 4
   } */
 
-  sortCards(cards: any[]) {
-    /* return cards.sort((a,b) => {
+  /* sortCards(cards: any[]) {
+    return cards.sort((a,b) => {
       if (a.suit === b.suit) {
         return this.valueOrder[b.value] - this.valueOrder[a.value]
       }
       return this.suitOrder[a.suit] - this.suitOrder[b.suit]
-    }) */
+    })
   }
 
   getRandomCards() {
-    /* const shuffled = [...this.cards].sort(() => 0.5 - Math.random())
+    const shuffled = [...this.cards].sort(() => 0.5 - Math.random())
 
     this.user_a.set(this.sortCards(shuffled.splice(0, 13)))
     this.user_b.set(this.sortCards(shuffled.splice(0, 13)))
     this.user_c.set(this.sortCards(shuffled.splice(0, 13)))
-    this.user_d.set(this.sortCards(shuffled.splice(0, 13))) */
+    this.user_d.set(this.sortCards(shuffled.splice(0, 13)))
   }
 
   dropCard(event: CdkDragDrop<string[]>) {
@@ -142,18 +127,44 @@ export class Game implements OnInit, OnDestroy {
       )
 
    }
-  }
+  } */
 
-  async chooseTrump() {
+  async chooseTrump(): Promise<Trump> {
     const ref = await this.dialog.open(TrumpModal, {
       disableClose: true,
     })
-    ref.afterClosed().subscribe((result: Trump) => {
-      this.trump.set(result)
-    })
+    return firstValueFrom(ref.afterClosed())
+  }
+
+  play(
+    event: CdkDragDrop<Card[]>,
+  ) {
+    const card = event.item.data as Card
+    const from = event.previousContainer.id as Seat
+    const to = event.container.id
+
+    if (to !== 'center') return
+
+    const allowed = this.validatorService.allowed(this.hands()[from], this.trickCards())
+    if (!allowed.includes(card)) return
+
+    this.hands.update(h => ({...h, [from]: h[from].filter(c => c !== card)}))
+
+    const result = this.trickService.playCard(from, card)
+    if (result) {
+      this.trick.set([])
+      console.log("Won the trick => ", result)
+    } else {
+      this.trick.update(t => [...t, {seat:from, card}])
+    }
+  }
+
+  dropToCenter(
+    event: CdkDragDrop<PlayedCard[]>,
+  ) {
+    event.event.preventDefault()
   }
 
   ngOnDestroy() {
-    //this.loading.set(true)
   }
 }
