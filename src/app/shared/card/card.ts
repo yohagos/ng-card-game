80ca78c8865cb371bfcf258206b5a90216cf2a91
yohagos <yosef.hagos@googlemail.com>
2025-10-11 21:36:03 +0200

    refactoring cards

--- src/app/shared/card/card.ts
+++ src/app/shared/card/card.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { CdkDrag } from '@angular/cdk/drag-drop';
+import { Component, computed, inject, Input } from '@angular/core';
+import { GameValidatorService } from '../../features/game/game-validator-service';
+import { Card } from '../../features/game/deck.service';
+
 
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [
+    CdkDrag,
+  ],
   templateUrl: './card.html',
   styleUrl: './card.scss'
 })
-export class Card {
+export class CardComponent {
+  private validator = inject(GameValidatorService)
 
+  @Input({required: true}) card!: Card
+  @Input({required: true}) hand!: Card[]
+  @Input({required: true}) trickCards!: Card[]
+
+  isAllowed = computed(() =>
+    this.validator.allowed(
+      this.hand,
+      this.trickCards,
+    ).includes(
+      this.card
+    )
+  )
 }
